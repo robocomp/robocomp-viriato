1f14bf68623a58fce079ff6649b04de6b5d4e34b
aracelivegamagro <aracelivegamagro@hotmail.com>
2019-01-23 12:35:08 +0100

    new component created

---
+++ components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMCommonBehavior.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/AGMCommonBehavior.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMExecutive.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/AGMExecutive.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMWorldModel.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/AGMWorldModel.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CommonBehavior.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/CommonBehavior.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/FaceTracking.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/FaceTracking.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/HumanTracker.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/HumanTracker.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/Planning.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/Planning.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/agmcommonbehaviorI.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/agmcommonbehaviorI.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/agmexecutivetopicI.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/agmexecutivetopicI.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/commonbehaviorI.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/commonbehaviorI.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/genericmonitor.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/genericmonitor.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/genericworker.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/genericworker.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/humanPose_autogen/mocs_compilation.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/humanPose_autogen/mocs_compilation.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/main.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/main.cpp.o"
+  "/opt/robocomp/classes/qlog/qlog.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/opt/robocomp/classes/qlog/qlog.cpp.o"
+  "/opt/robocomp/classes/rapplication/rapplication.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/opt/robocomp/classes/rapplication/rapplication.cpp.o"
+  "/opt/robocomp/classes/sigwatch/sigwatch.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/opt/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/specificmonitor.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/specificmonitor.cpp.o"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/src/specificworker.cpp" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CMakeFiles/humanPose.dir/specificworker.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_DLL"
+  "QT_GUI_LIB"
+  "QT_OPENGL_LIB"
+  "QT_XML_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/humanPose_autogen/include"
+  "src"
+  "../src/."
+  "/opt/robocomp/include"
+  "/classes"
+  "."
+  "/opt/robocomp/classes"
+  "/opt/robocomp/libs"
+  "/opt/robocomp/interfaces"
+  "/include"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtXml"
+  "/usr/include/qt4/QtCore"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMCommonBehavior.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMCommonBehavior.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMExecutive.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMExecutive.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMWorldModel.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/AGMWorldModel.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CommonBehavior.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/CommonBehavior.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/FaceTracking.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/FaceTracking.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/HumanTracker.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/HumanTracker.cpp"
+  "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/Planning.h" "/home/robolab/robocomp/components/robocomp-viriato/components/humanPose/cmake-build-debug/src/Planning.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ components/humanPose/src/specificworker.h
@@ -0,0 +1,137 @@
+/*
+ *    Copyright (C)2018 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+       \brief
+       @author authorname
+*/
+
+// THIS IS AN AGENT
+
+
+#ifndef SPECIFICWORKER_H
+#define SPECIFICWORKER_H
+
+#include <genericworker.h>
+#include <innermodel/innermodel.h>
+#ifdef USE_QTGUI
+	#include <osgviewer/osgview.h>
+	#include <innermodel/innermodelviewer.h>
+
+#endif
+
+class SpecificWorker : public GenericWorker
+{
+Q_OBJECT
+
+public:
+    struct Pose3D
+    {
+        float x;
+        float z;
+        float ry;
+        bool pos_good;
+        bool rot_good;
+        int confidence = 0;
+    };
+
+    struct PersonType
+    {
+        int id;
+        Pose3D pos;
+    };
+
+    vector<PersonType> list_of_humans;
+
+
+
+    vector <int> list_id;
+    vector <int32_t> list_psymbol;
+
+    int mesh = 1;
+
+    bool first = true;
+    bool facefound = false;
+
+
+    //----------------- intento de relacionar cara con esqueleto -----------------------//
+	typedef map <int,int> relID;
+
+	relID IDjointface; //mapa que relaciona los id procedentes del esqueleto con los de la cara
+	relID IDfacegeneric;// rel cara con id generico de humanPose
+	relID IDjointgeneric;//rel id joint con id generico
+
+	int IDgeneric = 0;
+    bool backwards = false;
+
+    //----------------------------------------------//
+
+    SpecificWorker(MapPrx& mprx);
+	~SpecificWorker();
+	bool setParams(RoboCompCommonBehavior::ParameterList params);
+	void includeInAGM(int id,Pose3D pose);
+	void movePersonInAGM(int id, Pose3D pose);
+    void getDataFromAstra();
+
+//	bool removeFromAGM(int id);
+
+    bool getPoseRot (jointListType list, Pose3D &personpose);
+    int getIDgeneric(int idjoint, RoboCompFaceTracking::Faces faces);
+
+
+
+    bool reloadConfigAgent();
+	bool activateAgent(const ParameterMap &prs);
+	bool setAgentParameters(const ParameterMap &prs);
+	ParameterMap getAgentParameters();
+	void killAgent();
+	int uptimeAgent();
+	bool deactivateAgent();
+	StateStruct getAgentState();
+	void structuralChange(const RoboCompAGMWorldModel::World &w);
+	void edgesUpdated(const RoboCompAGMWorldModel::EdgeSequence &modifications);
+	void edgeUpdated(const RoboCompAGMWorldModel::Edge &modification);
+	void symbolUpdated(const RoboCompAGMWorldModel::Node &modification);
+	void symbolsUpdated(const RoboCompAGMWorldModel::NodeSequence &modifications);
+
+
+
+public slots:
+	void compute();
+
+private:
+
+    QMutex *mux;
+	InnerModel *innerModel;
+
+#ifdef USE_QTGUI
+	OsgView *osgView;
+	InnerModelViewer *innerModelViewer;
+#endif
+	std::string action;
+	ParameterMap params;
+	AGMModel::SPtr worldModel;
+	bool active;
+	void regenerateInnerModelViewer();
+	bool setParametersAndPossibleActivation(const ParameterMap &prs, bool &reactivated);
+	bool sendModificationProposal(AGMModel::SPtr &worldModel, AGMModel::SPtr &newModel);
+
+};
+
+#endif
