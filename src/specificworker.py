6d5dcefdda4abb4436df26ddf2722b1d14d73041
Rahul Katiyar <rahulkatiyar19955@gmail.com>
2020-06-03 23:04:28 +0530

    implement the first part of the mockup in humanObserverAgent_GUI

--- src/specificworker.py
+++ src/specificworker.py
@@ -18,8 +18,14 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
-
+import PySide2
 from genericworker import *
+
+try:
+    from ui_activity_form import *
+except:
+    print("Can't import UI file. Did you run 'make'?")
+
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 # sys.path.append('/opt/robocomp/lib')
@@ -27,25 +33,71 @@ from genericworker import *
 # import librobocomp_osgviewer
 # import librobocomp_innermodel
 
+
+class OpenActivityForm(QDialog):
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.parent = parent
+        self.ui = Ui_activityForm()
+        self.ui.setupUi(self)
+        self.setModal(True)
+        self.ui.pushButton.clicked.connect(self.button_ok)
+        self.ui.pushButton_2.clicked.connect(self.button_cancel)
+        self.ui.comboBox_4.currentIndexChanged.connect(self.type_check)
+        self.ui.comboBox_5.activated.connect(self.location_check)
+        self.ui.textEdit_3.setText("Activity " + str(self.parent.ui.comboBox_3.count()+1))
+        self.ui.comboBox_6.addItems({"Stretcher", "Robot"})
+
+    # save the data when ok button is pressed
+    def button_ok(self):
+        print("ok button")
+        self.parent.ui.comboBox_3.addItem(self.ui.textEdit_3.toPlainText())
+        self.hide()
+
+    # exit the form and discard all the data
+    def button_cancel(self):
+        self.hide()
+
+    # check the type of the activity
+    def type_check(self, index):
+        if self.ui.comboBox_4.itemText(index) == "Individual":
+            self.ui.label_9.show()
+            self.ui.textEdit.show()
+        else:
+            self.ui.label_9.hide()
+            self.ui.textEdit.hide()
+
+    # check the location
+    def location_check(self, index):
+        if self.ui.comboBox_5.itemText(index) == "Physical therapy room":
+            self.ui.comboBox_6.clear()
+            self.ui.comboBox_6.addItems({"Stretcher", "Robot"})
+        else:
+            self.ui.comboBox_6.clear()
+            self.ui.comboBox_6.addItems({"Table", "Robot", "TV"})
+
+
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map):
         super(SpecificWorker, self).__init__(proxy_map)
         self.timer.timeout.connect(self.compute)
         self.Period = 2000
         self.timer.start(self.Period)
+        self.ui.pushButton_10.clicked.connect(self.newActivity)
 
+        # self.ui2 = Ui_activityForm()
+        # self.ui2.setupUi(self)
 
     def __del__(self):
         print('SpecificWorker destructor')
 
     def setParams(self, params):
-        #try:
+        # try:
         #	self.innermodel = InnerModel(params["InnerModelPath"])
-        #except:
+        # except:
         #	traceback.print_exc()
         #	print("Error reading config params")
         return True
-
 
     @QtCore.Slot()
     def compute(self):
@@ -66,77 +118,68 @@ class SpecificWorker(GenericWorker):
 
         return True
 
-
-
     #
     # SUBSCRIPTION to edgeUpdated method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_edgeUpdated(self, modification):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to edgesUpdated method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_edgesUpdated(self, modifications):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to selfEdgeAdded method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_selfEdgeAdded(self, nodeid, edgeType, attributes):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to selfEdgeDeleted method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_selfEdgeDeleted(self, nodeid, edgeType):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to structuralChange method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_structuralChange(self, w):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to symbolUpdated method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_symbolUpdated(self, modification):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     #
     # SUBSCRIPTION to symbolsUpdated method from AGMExecutiveTopic interface
     #
     def AGMExecutiveTopic_symbolsUpdated(self, modifications):
         #
-        #subscribesToCODE
+        # subscribesToCODE
         #
         pass
-
 
     # =============== Methods for Component Implements ==================
     # ===================================================================
@@ -151,7 +194,6 @@ class SpecificWorker(GenericWorker):
         #
         return ret
 
-
     #
     # deactivateAgent
     #
@@ -161,7 +203,6 @@ class SpecificWorker(GenericWorker):
         # implementCODE
         #
         return ret
-
 
     #
     # getAgentParameters
@@ -173,7 +214,6 @@ class SpecificWorker(GenericWorker):
         #
         return ret
 
-
     #
     # getAgentState
     #
@@ -184,7 +224,6 @@ class SpecificWorker(GenericWorker):
         #
         return ret
 
-
     #
     # killAgent
     #
@@ -193,7 +232,6 @@ class SpecificWorker(GenericWorker):
         # implementCODE
         #
         pass
-
 
     #
     # reloadConfigAgent
@@ -205,7 +243,6 @@ class SpecificWorker(GenericWorker):
         #
         return ret
 
-
     #
     # setAgentParameters
     #
@@ -215,7 +252,6 @@ class SpecificWorker(GenericWorker):
         # implementCODE
         #
         return ret
-
 
     #
     # uptimeAgent
@@ -230,4 +266,8 @@ class SpecificWorker(GenericWorker):
     # ===================================================================
     # ===================================================================
 
-
+    # ======functions for ui=============
+    def newActivity(self):
+        print("button Clicked2")
+        self.activityForm = OpenActivityForm(self)
+        self.activityForm.show()
---
+++ src/ui_mainUI.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainUI.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_guiDlg(object):
+    def setupUi(self, guiDlg):
+        if guiDlg.objectName():
+            guiDlg.setObjectName(u"guiDlg")
+        guiDlg.resize(649, 170)
+        self.dateTimeEdit = QDateTimeEdit(guiDlg)
+        self.dateTimeEdit.setObjectName(u"dateTimeEdit")
+        self.dateTimeEdit.setGeometry(QRect(40, 30, 151, 26))
+        self.label_9 = QLabel(guiDlg)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(40, 0, 151, 21))
+        self.label_22 = QLabel(guiDlg)
+        self.label_22.setObjectName(u"label_22")
+        self.label_22.setGeometry(QRect(10, 30, 21, 31))
+        self.pushButton_14 = QPushButton(guiDlg)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setGeometry(QRect(210, 30, 101, 23))
+        self.line_5 = QFrame(guiDlg)
+        self.line_5.setObjectName(u"line_5")
+        self.line_5.setGeometry(QRect(310, 0, 20, 91))
+        self.line_5.setFrameShape(QFrame.VLine)
+        self.line_5.setFrameShadow(QFrame.Sunken)
+        self.label_24 = QLabel(guiDlg)
+        self.label_24.setObjectName(u"label_24")
+        self.label_24.setGeometry(QRect(370, 0, 151, 21))
+        self.pushButton_10 = QPushButton(guiDlg)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.pushButton_10.setGeometry(QRect(330, 60, 91, 23))
+        self.pushButton_11 = QPushButton(guiDlg)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(430, 60, 91, 23))
+        self.comboBox_3 = QComboBox(guiDlg)
+        self.comboBox_3.setObjectName(u"comboBox_3")
+        self.comboBox_3.setGeometry(QRect(330, 30, 191, 22))
+
+        self.retranslateUi(guiDlg)
+
+        QMetaObject.connectSlotsByName(guiDlg)
+    # setupUi
+
+    def retranslateUi(self, guiDlg):
+        guiDlg.setWindowTitle(QCoreApplication.translate("guiDlg", u"humanObserverAgent_GUI", None))
+        self.label_9.setText(QCoreApplication.translate("guiDlg", u"<html><head/><body><p><span style=\" font-weight:600; color:#054e27;\">Date</span></p></body></html>", None))
+        self.label_22.setText(QCoreApplication.translate("guiDlg", u"<html><head/><body><p><img src=\":/GUI/Icons/calendar.png\" /></p></body></html>", None))
+        self.pushButton_14.setText(QCoreApplication.translate("guiDlg", u"View agenda", None))
+        self.label_24.setText(QCoreApplication.translate("guiDlg", u"<html><head/><body><p><span style=\" font-weight:600; color:#054e27;\">Activity agenda</span></p></body></html>", None))
+        self.pushButton_10.setText(QCoreApplication.translate("guiDlg", u"New activity", None))
+        self.pushButton_11.setText(QCoreApplication.translate("guiDlg", u"Set activity", None))
+    # retranslateUi
+
